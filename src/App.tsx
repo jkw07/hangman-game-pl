4714cc4d63e9f261e333c832481b51cbe729958e
Justyna W <jkwodecka@gmail.com>
2025-02-28 16:08:45 +0100

    change router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { ROUTES } from "./config/routes";
